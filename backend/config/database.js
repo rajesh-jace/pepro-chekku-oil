554e7ab38cf777f0b6b1421e906de0dd2cf4fdc6
rajesh-jace <j.rajesh256@gmail.com>
2024-01-18 20:26:32 +0530

    2nd commit
    port number and mongoose.set in mongo database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_LOCAL_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(con=>{
-        console.log(`MongoDB is connected to the host: ${con.connection.host} `)
+const connectDatabase = () => {
+  mongoose.set("strictQuery", false);
+  mongoose.connect(process.env.DB_LOCAL_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-}
+    .then((con) => {
+      console.log(`MongoDB is connected to the host: ${con.connection.host} `);
+    });
+};
 
 module.exports = connectDatabase;
